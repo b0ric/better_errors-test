11ec6decc93a02e0b41c7c7e51edbb7fa2fbff5f
Alex Borisov <b0ric.alex@gmail.com>
2014-05-20 21:24:32 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetterErrorsTest::Application.config.secret_key_base = '2a95e528664b40fba337c113cacd5fc7d0c34de4b11914276ea063cda18b183f1832d09243919ad5cc4e60eaef2f274e68c3dc829fc7a9a9ac97d295c66a9d13'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BetterErrorsTest::Application.config.session_store :cookie_store, key: '_better_errors-test_session'
